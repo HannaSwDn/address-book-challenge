5ef2a3735f9f142bafa73752cff5bfd1916f64b3
hanna_nyman <hannanyman.sk@gmail.com>
2018-12-09 11:45:10 +0100

    Adds a video to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'address-book-challenge';
+  title = 'app';
+
+    video = {
+    title: 'Despacito',
+    views: 2,
+    liked: true
+  }
+
+  handleClick(){
+    console.log('Someone clicked on button on the template :-)')
+  }
 }
